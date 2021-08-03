e5f3d0b64eed578aa3af792657be78af21e4c90a
leanix-nspataro <88043754+leanix-nspataro@users.noreply.github.com>
2021-08-03 07:55:42 -0400

    ldif json output
    added transform to ldif and json file output

--- sh_get_sheet.py
+++ sh_get_sheet.py
@@ -1,6 +1,26 @@
 import smartsheet
+import json
 
 smart = smartsheet.Smartsheet()
+
+## base json and field mapping
+base_json = {
+    "connectorType": "Smartsheet Test",
+    "connectorId": "smartSheet",
+	"connectorVersion": "1.0.0",
+	"lxVersion": "1.0.0",
+	"description": "",
+	"processingDirection": "inbound",
+	"processingMode": "partial",
+	"customFields": {},
+	"content": []
+}
+
+field_mapper = {
+    'Due Date': 'dueDate',
+    'Task Name': 'taskName',
+    'Assigned To': 'assignedTo'
+}
 
 ## returns id of all sheets
 def get_all_sheets(smart):
@@ -17,10 +37,12 @@ def get_all_sheets(smart):
     return all_sheets
 
 ## returns dict of sheet data to feed ldif
-def get_sheet_data(smart, da_sheets):
+def get_sheet_data(smart, all_sheets, base_json, field_mapper):
     all_columns = {}
     fact_sheet_output = {}
-    for a_sheet in da_sheets:
+    i = 1
+
+    for a_sheet in all_sheets:
         sheet_data = smart.Sheets.get_sheet(a_sheet) 
         # populate dict of column id:name 
         for a_column in sheet_data.columns:
@@ -30,17 +52,25 @@ def get_sheet_data(smart, da_sheets):
         for a_row in sheet_data.rows:
             row_data = {}
             row_data['type'] = 'Project'
+            row_data['id'] = 'PROJ-'+str(i)
+            row_data['data'] = {}
             all_cells = a_row.cells
             for a_cell in all_cells:
                 try:
-                    row_data[all_columns[str(a_cell._column_id)]] = a_cell._value
+                    if all_columns[str(a_cell._column_id)] in field_mapper:
+                        row_data['data'][field_mapper[all_columns[str(a_cell._column_id)]]] = a_cell._value
+                    else:
+                        row_data['data'][all_columns[str(a_cell._column_id)]] = a_cell._value
                 except KeyError:
                     pass
 
-            fact_sheet_output[row_data['Task Name']] = row_data
+            base_json['content'].append(row_data)
+            i += 1
 
-    return fact_sheet_output
+    return base_json
 
 if __name__ == '__main__':
-    da_sheets = get_all_sheets(smart)
-    get_sheet_data(smart, da_sheets)
+    all_sheets = get_all_sheets(smart)
+    ldif = get_sheet_data(smart, all_sheets, base_json, field_mapper)
+    with open('smartsheet_ldif.json', 'w') as fp:
+        json.dump(ldif, fp)
