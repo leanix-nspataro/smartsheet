4f41b00c760c490689e803e096bad305f763f54f
leanix-nspataro <88043754+leanix-nspataro@users.noreply.github.com>
2021-08-19 10:08:39 -0400

    fixed lean ix integration
    now works end to end, extract data from smartsheet and import to lean ix workspace

--- sh_get_sheet.py
+++ sh_get_sheet.py
@@ -2,13 +2,14 @@ import smartsheet
 from datetime import datetime
 import json
 import requests
+import re
 
-top_level = 'workspace'
+top_level = "workspace"
 
 field_mapper = {
-    'Due Date': 'dueDate',
-    'Task Name': 'taskName',
-    'Assigned To': 'assignedTo'
+    "Due Date": "dueDate",
+    "Task Name": "taskName",
+    "Assigned To": "assignedTo"
 }
 
 class smartsheetToLdif:
@@ -17,8 +18,8 @@ class smartsheetToLdif:
 
     ## base json and field mapping
     base_json = {
-        "connectorType": "Smartsheet Test",
-        "connectorId": "smartSheet",
+        "connectorType": "Smartsheet Test-2",
+        "connectorId": "smartSheet-2",
         "connectorVersion": "1.0.0",
         "lxVersion": "1.0.0",
         "description": "",
@@ -41,13 +42,13 @@ class smartsheetToLdif:
             wkspc_dict = wkspc_data.to_dict()
 
             # create map of sheets to workspaces
-            for a_sheet in wkspc_dict['sheets']:
-                str_id = str(a_sheet['id'])
+            for a_sheet in wkspc_dict["sheets"]:
+                str_id = str(a_sheet["id"])
                 new_wkspc = {}
                 new_wkspc[str_id] = {}
-                new_wkspc[str_id]['wkspc_id'] = str(wkspc.id)
-                new_wkspc[str_id]['wkspc_name'] = wkspc.name
-                new_wkspc[str_id]['type'] = 'workspace'
+                new_wkspc[str_id]["wkspc_id"] = str(wkspc.id)
+                new_wkspc[str_id]["wkspc_name"] = wkspc.name
+                new_wkspc[str_id]["type"] = "workspace"
                 self.sh_wkspc_map.update(new_wkspc)
 
         return self.wkspc_arr, self.sh_wkspc_map
@@ -59,18 +60,18 @@ class smartsheetToLdif:
         self.response = smart.Sheets.list_sheets(include_all=True)
         self.sheets = self.response.data
         self.all_sheets = []
-        # loop over sheets to get id's
+        # loop over sheets to get id"s
         for a_sheet in self.sheets:
             str_id = str(a_sheet.id)
             # example sheet: {"accessLevel": "OWNER", "createdAt": "2021-07-29T20:43:04+00:00Z", "id": 5473133349103492, "modifiedAt": "2021-07-30T14:04:39+00:00Z", "name": "projects", "permalink": "https://app.smartsheet.com/sheets/jfCMVXQ6jQF4H8PWJ3jGhJf75pxjwvX3VXv9CXV1"}
             row_data = {}
-            row_data['type'] = 'Sheet'
-            row_data['id'] = str_id
-            row_data['data'] = {}
-            row_data['data']['name'] = a_sheet.name
-            row_data['data']['createdAt'] = a_sheet._created_at.value.strftime('%Y-%m-%d')
+            row_data["type"] = "Sheet"
+            row_data["id"] = str_id
+            row_data["data"] = {}
+            row_data["data"]["name"] = a_sheet.name
+            row_data["data"]["createdAt"] = a_sheet._created_at.value.strftime("%Y-%m-%d")
             if str_id in sht_wkspc_map:
-                row_data['data']['parentId'] = sht_wkspc_map[str_id]['wkspc_id']
+                row_data["data"]["parentId"] = sht_wkspc_map[str_id]["wkspc_id"]
             self.all_sheets.append(row_data)
 
         return self.all_sheets
@@ -82,7 +83,7 @@ class smartsheetToLdif:
         self.i = 1
 
         for a_sheet in all_sheets:
-            sheet_data = smart.Sheets.get_sheet(int(a_sheet['id'])) 
+            sheet_data = smart.Sheets.get_sheet(int(a_sheet["id"])) 
             # populate dict of column id:name 
             for a_column in sheet_data.columns:
                 self.all_columns[str(a_column.id)] = a_column.title
@@ -90,23 +91,23 @@ class smartsheetToLdif:
             # build dict for each row
             for a_row in sheet_data.rows:
                 row_data = {}
-                row_data['type'] = 'Task'
-                row_data['id'] = str(a_row.id)
-                row_data['data'] = {}
-                row_data['data']['createdAt'] = str(a_row._created_at.value.strftime('%Y-%m-%d'))
-                row_data['data']['modifiedAt'] = str(a_row._modified_at.value.strftime('%Y-%m-%d'))
-                row_data['data']['modifiedBy'] = str(a_row._modified_by.value)
-                row_data['data']['parentId'] = str(a_row._parent_id)
-                row_data['data']['projectId'] = str(a_sheet['id'])
-                row_data['data']['projectName'] = str(sheet_data.name)
+                row_data["type"] = "Task"
+                row_data["id"] = str(a_row.id)
+                row_data["data"] = {}
+                row_data["data"]["createdAt"] = str(a_row._created_at.value.strftime("%Y-%m-%d"))
+                row_data["data"]["modifiedAt"] = str(a_row._modified_at.value.strftime("%Y-%m-%d"))
+                row_data["data"]["modifiedBy"] = str(a_row._modified_by.value)
+                row_data["data"]["parentId"] = str(a_row._parent_id)
+                row_data["data"]["projectId"] = str(a_sheet["id"])
+                row_data["data"]["projectName"] = str(sheet_data.name)
 
                 all_cells = a_row.cells
                 for a_cell in all_cells:
                     try:
                         if self.all_columns[str(a_cell._column_id)] in field_mapper:
-                            row_data['data'][field_mapper[self.all_columns[str(a_cell._column_id)]]] = a_cell._value
+                            row_data["data"][field_mapper[self.all_columns[str(a_cell._column_id)]]] = a_cell._value
                         else:
-                            row_data['data'][self.all_columns[str(a_cell._column_id)]] = a_cell._value
+                            row_data["data"][self.all_columns[str(a_cell._column_id)]] = a_cell._value
                     except KeyError:
                         pass
 
@@ -119,37 +120,36 @@ class smartsheetToLdif:
         if workspaces != "None":
             for wrkspc in workspaces:
                 new_content = {}
-                new_content['type'] = 'Workspace'
-                new_content['id'] = wrkspc[0]
-                new_content['data'] = {}
-                new_content['data']['name'] = wrkspc[1]
-                base_json['content'].append(new_content)
+                new_content["type"] = "Workspace"
+                new_content["id"] = wrkspc[0]
+                new_content["data"] = {}
+                new_content["data"]["name"] = wrkspc[1]
+                base_json["content"].append(new_content)
 
         for a_sheet in all_sheets:
-            base_json['content'].append(a_sheet)
+            base_json["content"].append(a_sheet)
 
         for a_task in all_tasks:
-            base_json['content'].append(a_task)
+            base_json["content"].append(a_task)
 
         return base_json
 
 class ldifToWorkspace():
-    api_token = 'AUxw58vgdwnbBD4dbk9yWjDkEeWh5KWNCb9FvvG9'
-    auth_url = 'https://demo-us.leanix.net/services/mtm/v1/oauth2/token'
-    request_url = 'https://demo-us.leanix.net/services/integration-api/v1/'
+    api_token = ""
+    auth_url = "https://demo-us.leanix.net/services/mtm/v1/oauth2/token"
+    request_url = "https://demo-us.leanix.net/services/integration-api/v1/"
 
-    response = requests.post(auth_url, auth=('apitoken', api_token), data={'grant_type': 'client_credentials'})
+    response = requests.post(auth_url, auth=("apitoken", api_token), data={"grant_type": "client_credentials"})
     response.raise_for_status()
-    header = {'Authorization': 'Bearer ' + response.json()['access_token'], 'Content-Type': 'application/json'}
+    header = {"Authorization": "Bearer " + response.json()["access_token"], "Content-Type": "application/json"}
 
-    with open('sh_processor4.json') as json_file:
+    with open("/Users/nathanielspataro/Documents/smartsheet/sh_processor4.json") as json_file:
         ldif_processor = json.load(json_file)
-    ldif_proc_json = json.dumps(ldif_processor)
 
-    def createProcessorRun(self, ldif_proc_json, request_url, header, api_token):
+    def createProcessorRun(self, ldif_processor, request_url, header, api_token):
         self.data = {
-            "connectorType": "Smartsheet Test",
-            "connectorId": "smartSheet",
+            "connectorType": "Smartsheet Test-3",
+            "connectorId": "smartSheet-3",
             "connectorVersion": "1.0.0",
             "processingDirection": "inbound",
             "processingMode": "partial",
@@ -161,38 +161,36 @@ class ldifToWorkspace():
                     "default": "0"
                 }
             },
-            "processors": ldif_proc_json
+            "processors": ldif_processor['processors']
         }
-
-        # print(data)
-        response = requests.put(url=request_url + 'configurations/', headers=header, data=json.dumps(self.data))
+        self.data = json.dumps(self.data)
+        new_data = re.sub(r': True', ': true', self.data)
+        response = requests.put(url=request_url + "configurations/", headers=header, data=new_data)
 
     def createRun(self, content, request_url, header):
         self.data = {
-            "connectorType": "Smartsheet Test",
-            "connectorId": "smartSheet",
+            "connectorType": "Smartsheet Test-3",
+            "connectorId": "smartSheet-3",
             "connectorVersion": "1.0.0",
             "lxVersion": "1.0.0",
             "description": "Creates LeanIX Projects from Smartsheet workspaces",
             "processingDirection": "inbound",
             "processingMode": "partial",
-            "customFields": "",
-            "content": content
+            "customFields": {},
+            "content": content['content']
         }
-
-        # print(data)
-        self.response = requests.post(url=request_url + 'synchronizationRuns/', headers=header, data=json.dumps(self.data))
-        # print(response.json())
+        
+        self.response = requests.post(url=request_url + "synchronizationRuns/", headers=header, data=json.dumps(self.data))
         return (self.response.json())
 
     def startRun(self, run, request_url, header):
-        response = requests.post(url=request_url + 'synchronizationRuns/' + run['id'] + '/start?test=false', headers=header)
+        response = requests.post(url=request_url + "synchronizationRuns/" + run["id"] + "/start?test=false", headers=header)
 
     def status(run, request_url, header):
-        response = requests.get(url=request_url + 'synchronizationRuns/' + run['id'] + '/status', headers=header)
+        response = requests.get(url=request_url + "synchronizationRuns/" + run["id"] + "/status", headers=header)
         return (response.json())
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     #### Get smartsheet data, transform to LDIF
     sh = smartsheetToLdif()
     ## get data from Smartsheet
@@ -202,19 +200,15 @@ if __name__ == '__main__':
     ## transform data to LDIF
     ldif_output = sh.transform_to_ldif(sh.base_json, all_sheets, all_tasks, workspaces)
     ## write LDIF to json
-    with open('smartsheet_ldif5.json', 'w') as fp:
+    with open("/Users/nathanielspataro/Documents/smartsheet/smartsheet_ldif5.json", "w") as fp:
         json.dump(ldif_output, fp)
 
 
     #### send data to Lean IX workspace
     lx = ldifToWorkspace()
-    lx.createProcessorRun(lx.ldif_proc_json, lx.request_url, lx.header, lx.api_token)
-    
-    reform_ldif = json.dumps(ldif_output)
-    # print(reform_ldif)
-    run = lx.createRun(reform_ldif, lx.request_url, lx.header)
-    print(run['message'])
+    lx.createProcessorRun(lx.ldif_processor, lx.request_url, lx.header, lx.api_token)
+    run = lx.createRun(ldif_output, lx.request_url, lx.header)
     lx.startRun(run, lx.request_url, lx.header)
     print("Started run...")
     while (True):
-         if (lx.status(run, lx.request_url, lx.header)['status'] == 'FINISHED'): break
+         if (lx.status(run, lx.request_url, lx.header)["status"] == "FINISHED"): break
